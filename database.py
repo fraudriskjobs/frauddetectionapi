eeb6bad37d82f41090ac18e4cdde8698b791bdbd
fraudriskjobs <aswin.sil@gmail.com>
2025-03-31 18:15:45 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,21 +1,13 @@
+# In your database.py or main.py
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from config import settings
+import os
 
-SQLALCHEMY_DATABASE_URL = (
-    f"postgresql://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@"
-    f"{settings.POSTGRES_SERVER}:{settings.POSTGRES_PORT}/{settings.POSTGRES_DB}"
-)
+# Use Render's environment variable
+SQLALCHEMY_DATABASE_URL = os.getenv(postgresql://frauddetection_db_user:oUYa375Th2tSSsCimytrTlGwOj9KOjLB@dpg-cvl876s9c44c73fblnog-a/frauddetection_db)  # Render auto-injects this
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+        
